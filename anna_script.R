0f101a9fc3b14e03e26a1799839a6f12b1e7ce43
Anna Moeller <anna.moeller@umontana.edu>
2019-09-23 15:05:03 -0600

    Tried to create Rproj

--- anna_script.R
+++ anna_script.R
@@ -1,6 +1,6 @@
 # Anna words
 
-dat <- tibble(name = "Anna", 
+dat <- tibble::tibble(name = "Anna", 
               word = c(
                 "unmarked", "abundance", "sampling", "demographics", "time-to-event",
                 "distribution", "cameras", "population", "mule deer", 
@@ -8,7 +8,7 @@ dat <- tibble(name = "Anna",
                 "quantitative", "space-to-event", "modeling", "survival", 
                 "spatiotemporal", "habitat quality", "spatial scale", "spaceNtime",
                 "population dynamics", "harvest", "conservation", "private lands", 
-                "collars"
+                "collars", "JAGS"
               )
         )
 save(dat, file = paste(here::here(), "data/moeller.RData", sep = "/"))
